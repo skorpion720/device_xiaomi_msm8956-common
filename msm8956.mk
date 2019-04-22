2826933a09fe626d90116cbf316de6d89a9ca5ca
pratik2799 <pithore.pratik@gmail.com>
2019-04-22 17:44:51 +0530

    msm8956-common: Build libsensorndkbridge
    * This can be build from source
    
    Signed-off-by: pratik2799 <pithore.pratik@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -369,7 +369,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Shim
 PRODUCT_PACKAGES += \

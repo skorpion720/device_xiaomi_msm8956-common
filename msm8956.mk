9a5e625b1ab5d79b4549b689083d219cde690eaf
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-20 05:17:12 +0200

    Revert "msm8956: Remove hidden API references in ims.apk"
    This reverts commit 88e084f4485db28f8e11abd35947bf209692af8c.

--- msm8956.mk
+++ msm8956.mk
@@ -390,10 +390,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
